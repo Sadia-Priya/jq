23160d039490283c7ceb83e88029c7973ef07252
SadiaPriya <sadiapriyabd@gmail.com>
2024-11-16 19:20:29 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+alert('Welcome');
+});
+
+
+$(document).ready(function(){
+	$('button').click(function(){
+		alert('Welcome');
+	});
+
+});
+
+
+$(document).ready(function(){
+	$('#btn').on('click',function(){
+		alert('Welcome Sadia');
+	});
+
+});
+
+
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
